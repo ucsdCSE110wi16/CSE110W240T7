2232ebddb1a32c9bdd9751e6d3b8e6f300a01ded
wh2ryan <wang.haoran@icloud.com>
2016-02-21 23:44:37 -0800

    Progress bar

--- MyApplication/app/src/main/java/ui/Homepage.java
+++ MyApplication/app/src/main/java/ui/Homepage.java
@@ -1,6 +1,7 @@
 package ui;
 
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.util.Log;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ProgressBar;
 
 import androidstudio.edbud.com.myapplication.R;
 import model.IndividualAssignment;
@@ -18,15 +20,18 @@ import ui.BaseActivity;
 public class Homepage extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navi);
+
         super.onCreateNavigation();
-
-
+        ProgressBar progress_bar = (ProgressBar)findViewById(R.id.circle_progress_bar);
+        progress_bar.setProgress(35);
+        TextView gpanumber = (TextView)findViewById(R.id.GPAnumber);
+        Double gpa = 3.5;
+        String str = String.valueOf(gpa);
+        gpanumber.setText(str);
         ListView recentDueList = (ListView) findViewById(R.id.list_homepage);
         HomepageListAdapter adapter = new HomepageListAdapter(this, user.recentDues);
         recentDueList.setAdapter(adapter);
@@ -45,5 +50,7 @@ public class Homepage extends BaseActivity
     public void onBackPressed(){
         // DO NOTHING
     }
+
+
 }
 
