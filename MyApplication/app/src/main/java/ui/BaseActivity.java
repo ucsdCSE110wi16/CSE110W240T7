f7a92aebc72f714ce6b69c526178a93026f0a665
creekhu <creekthu@gmail.com>
2016-03-06 13:58:42 -0800

    Change Register Phrasing

--- MyApplication/app/src/main/java/ui/BaseActivity.java
+++ MyApplication/app/src/main/java/ui/BaseActivity.java
@@ -62,8 +62,10 @@ public class BaseActivity extends AppCompatActivity
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
